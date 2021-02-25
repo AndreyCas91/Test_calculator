7dc3980887f91901da670e61a9bfa91f4dc40d47
Андрей <Grishanin.1991@mail.ru>
2021-02-25 20:43:11 +0700

    HW5

--- app/src/main/java/ru/geekbrains/test_calculator/MainActivity.java
+++ app/src/main/java/ru/geekbrains/test_calculator/MainActivity.java
@@ -2,7 +2,11 @@ package ru.geekbrains.test_calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("test://calculator");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
     }
 }
